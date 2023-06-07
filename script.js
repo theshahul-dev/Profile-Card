55c766bb79f3071a79b86556623533ed5bcf44f5
theshahul-dev <theshahuldev@gmail.com>
2023-06-07 20:44:04 +0530

    script deleted

--- script.js
+++
@@ -1,9 +0,0 @@
-// heart = document.getElementById("heart")
-// heart.addEventListener("click",clickHandler)
-// function clickHandler(){
-//     if(heart.classList.contains("color-change")){
-//         heart.classList.remove("color-change")
-//     }else{
-//         heart.classList.add("color-change")
-//     }
-// }
