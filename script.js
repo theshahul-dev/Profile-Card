9d4ecd6cedc17d1b33cae9c7e131190d970ae631
theshahul-dev <theshahuldev@gmail.com>
2023-06-07 16:57:55 +0530

    design changed

--- script.js
+++ script.js
@@ -1,9 +1,9 @@
-heart = document.getElementById("heart")
-heart.addEventListener("click",clickHandler)
-function clickHandler(){
-    if(heart.classList.contains("color-change")){
-        heart.classList.remove("color-change")
-    }else{
-        heart.classList.add("color-change")
-    }
-}
+// heart = document.getElementById("heart")
+// heart.addEventListener("click",clickHandler)
+// function clickHandler(){
+//     if(heart.classList.contains("color-change")){
+//         heart.classList.remove("color-change")
+//     }else{
+//         heart.classList.add("color-change")
+//     }
+// }
